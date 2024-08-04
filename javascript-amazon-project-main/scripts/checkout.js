e661ab39ac2f36097d151c89ff9c5e4a6b2f12ea
bmwolfe <wolfebrendon@gmail.com>
2024-08-04 13:27:13 -0400

    Generate order summary HTML

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,3 +1,6 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSUmmary.js";
 
 renderOrderSummary();
+
+renderPaymentSummary();
--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 
 //document.querySelector('.checkout-item-count').innerHTML = `${cart.quantity} items`;
 
@@ -18,24 +18,15 @@ export function renderOrderSummary(){
     cart.forEach((cartItem)=>{
         const productId = cartItem.productId;
 
-        let matchingProduct;
+        const matchingProduct = getProduct(productId);
 
-        products.forEach((product)=>{
-            if(product.id===productId){
-                matchingProduct=product;
-            }
-        });
+        const deliveryOptionId = cartItem.deliveryOptionId;
 
-        const deliveryOptionId = cartItem.
-        deliveryOptionId;
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-        let deliveryOption;
-
-        deliveryOptions.forEach((option)=>{
-            if(option.id === deliveryOptionId){
-                deliveryOption = option;
-            }
-        });
+        const today = dayjs();
+        const deliveryDate = today.add(7, 'days');
+        const dateString = deliveryDate.format('dddd, MMMM D');
 
         cartSummaryHTML += `
         <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
